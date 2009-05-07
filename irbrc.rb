082858fb337bfc5de3e9bd521479701f3fb704f7
Gabriel Horner <gabriel.horner@gmail.com>
2009-05-06 20:22:07 -0400

    pulled out irb manager to iam

--- irbrc.rb
+++ irbrc.rb
@@ -1,29 +1,15 @@
-# This irbrc provides a simple way to load preferred irb settings via
-# a methods in Iam::Libraries or a file under your base directory.
+# This irbrc uses my irb manager, iam, to load libraries of irb commands + snippets
 
 require 'rubygems'
-# attempt to load a local hirb gem
-begin
-  require 'local_gem' # gem install cldwalker-local_gem
-  LocalGem.local_require 'hirb' # gem install cldwalker-hirb
-rescue LoadError
-  require 'hirb' # gem install cldwalker-hirb
+%w{hirb alias iam}.each do |e|
+  # load a local gem first or default to normal gem
+  begin
+    require 'local_gem' # gem install cldwalker-local_gem
+    LocalGem.local_require e
+  rescue LoadError
+    require e # gem install cldwalker-#{e}
+  end
 end
-
-# attempt to load a local alias gem
-begin
-  require 'local_gem' # gem install cldwalker-local_gem
-  LocalGem.local_require 'alias' # gem install cldwalker-alias
-rescue LoadError
-  require 'alias' # gem install cldwalker-alias
-end
-
-#Set this to your preferred directory
-irb_base_dir = "#{ENV['HOME']}/.irb"
-irb_base_dir = File.exists?(irb_base_dir) ? irb_base_dir : '.irb'
-$:.unshift irb_base_dir
-require 'lib/iam'
-require 'libraries'
 
 Iam.register(:irb_options, :railsrc, :aliases, :history, :local_gem, :core_extensions, :wirble, :duration,
   :utility_belt, :history_commands, :tree_commands, Iam::Commands, Hirb::Console, :with=>self)
