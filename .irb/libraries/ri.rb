bdf459baf076468f192bdd324f3fcfa340b6880d
Gabriel Horner <gabriel.horner@gmail.com>
2009-06-05 06:29:34 -0400

    changed ri lib to use dynamic methods + optional regex

--- .irb/libraries/ri.rb
+++ .irb/libraries/ri.rb
@@ -5,30 +5,27 @@ module Ri
     require 'libraries/hirb'
   end
 
-  def ri(query)
-    query = query.to_s
+  def ri(original_query, regex=nil)
+    query = original_query.to_s
     if query =~ /::|\#|\./
       system_ri(query)
     else
-      ri_driver = RDoc::RI::Driver.new(RDoc::RI::Driver.process_args([query]))
-      if ri_driver.class_cache.key?(query)
-        ri_driver.display_class(query)
-        if (class_cache = ri_driver.class_cache[query])
-          methods = []
-          class_cache["class_methods"].map {|e| e["name"]}.each {|e|
-            methods << {:name=>"#{query}.#{e}", :type=>:class}
-          }
-          class_cache["instance_methods"].map {|e| e["name"]}.each {|e|
-            methods << {:name=>"#{query}.#{e}", :type=>:instance}
-          }
-          ::Hirb::Helpers::Menu.render(methods, :fields=>[:name, :type]) do |chosen|
-            system_ri(*chosen.map {|e| e[:name]})
-          end
+      if original_query.is_a?(Class) && regex
+        methods = []
+        original_query.methods(nil).grep(regex).sort.each {|e| methods << {:name=>"#{original_query}::#{e}", :type=>:class} }
+        original_query.instance_methods(nil).grep(regex).sort.each {|e| methods << {:name=>"#{original_query}##{e}", :type=>:instance} }
+        ::Hirb::Helpers::Menu.render(methods, :fields=>[:name, :type]) do |chosen|
+          system_ri(*chosen.map {|e| e[:name]})
         end
       else
-        results = ri_driver.select_methods(/#{query}/)
-        ::Hirb::Helpers::Menu.render(results, :fields=>['full_name']) do |chosen|
-          system_ri(*chosen.map {|e| e['full_name']})
+        ri_driver = RDoc::RI::Driver.new(RDoc::RI::Driver.process_args([query]))
+        if ri_driver.class_cache.key?(query)
+          ri_driver.display_class(query)
+        else
+          results = ri_driver.select_methods(/#{query}/)
+          ::Hirb::Helpers::Menu.render(results, :fields=>['full_name']) do |chosen|
+            system_ri(*chosen.map {|e| e['full_name']})
+          end
         end
       end
     end
