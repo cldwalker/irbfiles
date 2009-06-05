bfc890c1aa5997bda7cae642e76fdea91294b8cd
Gabriel Horner <gabriel.horner@gmail.com>
2009-06-05 05:52:51 -0400

    improved ri lib to have menu for a class' methods, catch aborts, take constants

--- .irb/libraries/ri.rb
+++ .irb/libraries/ri.rb
@@ -6,16 +6,29 @@ module Ri
   end
 
   def ri(query)
+    query = query.to_s
     if query =~ /::|\#|\./
       system_ri(query)
     else
       ri_driver = RDoc::RI::Driver.new(RDoc::RI::Driver.process_args([query]))
       if ri_driver.class_cache.key?(query)
         ri_driver.display_class(query)
+        if (class_cache = ri_driver.class_cache[query])
+          methods = []
+          class_cache["class_methods"].map {|e| e["name"]}.each {|e|
+            methods << {:name=>"#{query}.#{e}", :type=>:class}
+          }
+          class_cache["instance_methods"].map {|e| e["name"]}.each {|e|
+            methods << {:name=>"#{query}.#{e}", :type=>:instance}
+          }
+          ::Hirb::Helpers::Menu.render(methods, :fields=>[:name, :type]) do |chosen|
+            system_ri(*chosen.map {|e| e[:name]})
+          end
+        end
       else
         results = ri_driver.select_methods(/#{query}/)
-        if (chosen = ::Hirb::Helpers::Menu.render(results, :fields=>['full_name'], :choose=>:one))
-          system_ri(chosen['full_name'])
+        ::Hirb::Helpers::Menu.render(results, :fields=>['full_name']) do |chosen|
+          system_ri(*chosen.map {|e| e['full_name']})
         end
       end
     end
@@ -24,5 +37,6 @@ module Ri
 
   def system_ri(*queries)
     RDoc::RI::Driver.run(queries)
+  rescue SystemExit
   end
 end
