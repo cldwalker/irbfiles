19a5b69ab1aa7b8e53c5f66408e8c0a3a7f8ee05
Gabriel Horner <gabriel.horner@gmail.com>
2009-07-08 05:06:11 -0400

    several more handy gem methods, added my gem config

--- .irb/libraries/gem.rb
+++ .irb/libraries/gem.rb
@@ -10,13 +10,15 @@ module Gem
   end
 
   def gem_deps(name)
-    if shell('gem','dependency',name) =~ /(Gem #{name}-.*?)(Gem|\z)/m
-      $1.split("\n").grep(/runtime\s*\)/).map do |line|
-        line[/[\w-]+/]
-      end.compact
+    if (latest = latest_gem(name))
+      latest.dependencies.select {|e| e.type == :runtime }.map {|e| e.name}
     else
       []
     end
+  end
+
+  def latest_gem(name)
+    ::Gem.source_index.gems.values.select {|e| e.name == name }.sort_by {|e| e.version }[-1]
   end
 
   def gem_recursive_uninstall(name)
@@ -33,7 +35,17 @@ module Gem
 
   def gem_check
     gem_file = File.join(Boson.base_dir, 'config', 'gems.yml')
-    gem_list - YAML::load_file(gem_file)[:approved]
+    yaml = YAML::load_file(gem_file)
+    gem_list - yaml[:approved] - yaml[:system] - yaml[:approved].map {|e| gem_recursive_deps(e)}.flatten - yaml[:exception]
+  end
+
+  def gem_rdep(name)
+    ::Gem.source_index.gems.values.select {|e| e.dependencies.any? {|e| e.name == name && e.type == :runtime}}.
+      map {|e| "#{e.name}-#{e.version}" }
+  end
+
+  def gem_recursive_deps(name)
+    gem_deps(name).map {|e| [e] + gem_recursive_deps(e) }.flatten.uniq
   end
 
   def gem_save(gems=gem_list)
