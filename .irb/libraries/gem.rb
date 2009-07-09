075e3236b4449c0975f8d31e01b32c0c9e790f2d
Gabriel Horner <gabriel.horner@gmail.com>
2009-07-09 00:30:41 -0400

    more gem commands

--- .irb/libraries/gem.rb
+++ .irb/libraries/gem.rb
@@ -29,18 +29,30 @@ module Gem
       puts("Uninstall #{name} with dependencies: #{deps.join(', ')}? [y/n]")
       if gets.chomp[/y/]
         system(*(%w{sudo gem uninstall} + [name] + deps))
+      else
+        false
       end
     end
   end
 
   def gem_approved
-    gem_file = File.join(Boson.base_dir, 'config', 'gems.yml')
-    YAML::load_file(gem_file)[:approved]
+    table gem_config[:approved]
+  end
+
+  def gem_add(name)
+    gem_save(gem_config[:approved] << name)
+  end
+
+  def gem_remove(name)
+    if gem_recursive_uninstall(name)
+      approved = gem_config[:approved]
+      approved.delete(name.to_s)
+      gem_save(approved)
+    end
   end
 
   def gem_check
-    gem_file = File.join(Boson.base_dir, 'config', 'gems.yml')
-    yaml = YAML::load_file(gem_file)
+    yaml = gem_config
     gem_list - yaml[:approved] - yaml[:system] - yaml[:approved].map {|e| gem_recursive_deps(e)}.flatten - yaml[:exception]
   end
 
@@ -53,9 +65,17 @@ module Gem
     gem_deps(name).map {|e| [e] + gem_recursive_deps(e) }.flatten.uniq
   end
 
-  def gem_save(gems=gem_list)
-    gem_file = File.join(Boson.base_dir, 'config', 'gems.yml')
-    File.open(gem_file, 'w') {|f| f.write({:approved=>gems}.to_yaml) }
+  def gem_save(approved_gems)
+    new_config = gem_config.merge(:approved=>approved_gems)
+    File.open(gem_file, 'w') {|f| f.write(new_config.to_yaml) }
+  end
+
+  def gem_config
+    YAML::load_file(gem_file)
+  end
+
+  def gem_file
+    File.join(Boson.base_dir, 'config', 'gems.yml')
   end
 
   def gem_list(query='')
