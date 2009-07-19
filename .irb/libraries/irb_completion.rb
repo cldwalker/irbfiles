db1bc8abfba8c4f556c04448f9b2ef410d955e15
Gabriel Horner <gabriel.horner@gmail.com>
2009-07-19 17:05:06 -0400

    added shell commands, changing bond config style

--- .irb/libraries/irb_completion.rb
+++ .irb/libraries/irb_completion.rb
@@ -1,40 +1,49 @@
-module IrbCompletion
+module Bond
+  module Actions
+    def alias_constants(input)
+      fetch_constants = proc {|klass, klass_alias| klass.constants.grep(/^#{klass_alias}/i).map {|f| klass.const_get(f)} }
+      fetch_string_constants = proc {|klass, klass_alias|
+        klass.constants.grep(/^#{klass_alias}/i).map {|f|
+          (val = klass.const_get(f)) && val.is_a?(Module) ? val.to_s : "#{klass}::#{f}"
+        }
+      }
+
+      index = 1
+      aliases = input.split(":")
+      aliases.inject([Object]) do |completions,a|
+        completions = completions.select {|e| e.is_a?(Module) }.map {|klass|
+          aliases.size != index ? fetch_constants.call(klass, a) : fetch_string_constants.call(klass, a)
+        }.flatten
+        index += 1; completions
+      end
+    end
+
+    def shell_commands(input)
+      ENV['PATH'].split(File::PATH_SEPARATOR).uniq.map {|e| Dir.entries(e) }.flatten.uniq
+    end
+  end
+end
+
+module Boson::Libraries::IrbCompletion
   def load_bond
     begin LocalGem.local_require 'bond'; rescue; require 'bond' end
     Bond.reset
     Bond.debrief :debug=>true
-    Bond.complete(:method=>"reload") {|e,f|
-      $".map {|g| g.gsub('.rb','') }
+    Bond.complete(:method=>"reload") {|e|
+      $".map {|f| f.gsub('.rb','') }
     }
-    Bond.complete(:method=>/ll|rl/) {|e,f|
+    Bond.complete(:method=>/ll|rl/) {|e|
       Dir["#{Boson.base_dir}/libraries/**/*.rb"].map {|l| l[/#{Boson.base_dir}\/libraries\/(.*)\.rb/,1]}
     }
+    Bond.complete(:method=>/system|`/, :action=>:shell_commands)
     Bond.complete(:object=>"Object", :search=>:underscore)
-    Bond.complete(:on=>/^((([a-z][^:.\(]*)+):)+/, :search=>false, &method(:alias_constants))
+    Bond.complete(:on=>/^((([a-z][^:.\(]*)+):)+/, :search=>false, :action=>:alias_constants)
 
     # Bond.complete(:on=>/\S+\s*["']([^'"]*)$/, :search=>false) {|e|
     #   (Readline::FILENAME_COMPLETION_PROC.call(e.matched[1]) || []).map {|f|
     #     f =~ /^~/ ?  File.expand_path(f) : f
     #   }
     # }
-  end
-
-  def alias_constants(input)
-    fetch_constants = proc {|klass, klass_alias| klass.constants.grep(/^#{klass_alias}/i).map {|f| klass.const_get(f)} }
-    fetch_string_constants = proc {|klass, klass_alias|
-      klass.constants.grep(/^#{klass_alias}/i).map {|f|
-        (val = klass.const_get(f)) && val.is_a?(Module) ? val.to_s : "#{klass}::#{f}"
-      }
-    }
-
-    index = 1
-    aliases = input.split(":")
-    aliases.inject([Object]) do |completions,a|
-      completions = completions.select {|e| e.is_a?(Module) }.map {|klass|
-        aliases.size != index ? fetch_constants.call(klass, a) : fetch_string_constants.call(klass, a)
-      }.flatten
-      index += 1; completions
-    end
   end
 
   def irb_enhanced
