d192c373a175b8fa80f1ac833bd9be20a0352929
Gabriel Horner <gabriel.horner@gmail.com>
2009-07-31 01:32:05 -0400

    toggle completion method

--- .irb/libraries/irb_completion.rb
+++ .irb/libraries/irb_completion.rb
@@ -42,4 +42,18 @@ module Boson::Libraries::IrbCompletion
     Bond.complete(:method=>'r', :action=>:method_require, :search=>false)
     Bond.complete(:on=>/ENV\[["'](\S*)$/, :search=>false) {|e| ENV.keys.grep(/^#{Regexp.escape(e.matched[1])}/i) }
   end
+
+  def toggle_object_complete
+    if @object_complete
+      Bond.recomplete(:object=>'Object', :place=>:last)
+      Bond.recomplete(:object=>'Object', :on=>/([^.\s]+)\.([^.\s]*)$/, :place=>:last)
+    else
+      non_inherited_methods = proc {|e|
+        e.object.is_a?(Module) ? e.object.methods(false) : e.object.class.instance_methods(false)
+      }
+      Bond.recomplete(:object=>'Object', :place=>:last, &non_inherited_methods)
+      Bond.recomplete(:object=>'Object', :on=>/([^.\s]+)\.([^.\s]*)$/, :place=>:last, &non_inherited_methods)
+    end
+    @object_complete = !@object_complete
+  end
 end
