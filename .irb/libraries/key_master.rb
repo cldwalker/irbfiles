d2679f233705599efd65fff465ec4a4df4a363fb
Gabriel Horner <gabriel.horner@gmail.com>
2009-08-03 21:30:27 -0400

    unfinished lib for analyzing readline keybindings

---
+++ .irb/libraries/key_master.rb
@@ -0,0 +1,29 @@
+module KeyMaster
+  def keys_used
+    table functions_hash.to_a.sort, :headers=>%w{function key}, :filters=>{1=>proc {|e| e.join(",")}}
+  end
+
+  def functions_hash
+    @functions_hash ||= begin
+      hash = IO.readlines(File.expand_path("~/.key_bindings")).select {|e| e =~ /^[^\n#]/ }.
+        inject({}) {|h,e| k,v = e.chomp.split(/\s*:\s*/); (h[v] ||= []) << k ; h}
+      hash.delete(nil)
+      hash
+    end
+  end
+
+  def bash_functions
+    (keys_hash.values.uniq.compact - rline_functions).sort
+  end
+
+  def rline_functions
+    IO.readlines(File.expand_path("~/.key_bindings_rline")).map {|e| e[/^\S+/]}
+  end
+
+  def keys_hash
+    @keys_hash ||= begin
+      arr = IO.readlines(File.expand_path("~/.key_bindings")).select {|e| e =~ /^[^\n#]/ }.
+      inject({}) {|t,e| k,v = e.chomp.split(/\s*:\s*/); t[k] = v; t }
+    end
+  end
+end
