52e2e4ad20994c9d110a2228ec9d4cf55439f706
Gabriel Horner <gabriel.horner@gmail.com>
2009-08-05 05:24:58 -0400

    renamed .irb to boson

--- symlink_files.rb
+++ symlink_files.rb
@@ -4,4 +4,4 @@ require 'fileutils'
 home = File.expand_path('~')
 
 FileUtils.ln_s(File.join(Dir.pwd,'irbrc.rb'), File.join(home,'.irbrc'), :verbose=>true, :force=>true)
-FileUtils.ln_s(File.join(Dir.pwd,'.irb'), File.join(home,'.irb'), :verbose=>true, :force=>true)
+FileUtils.ln_s(File.join(Dir.pwd,'boson'), File.join(home,'.boson'), :verbose=>true, :force=>true)
