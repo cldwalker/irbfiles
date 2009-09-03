820504d22865519affcbde532f3067c6ea5975be
Gabriel Horner <gabriel.horner@gmail.com>
2009-09-03 17:38:21 -0400

    config tweaks

--- boson/commands/gsub.rb
+++ boson/commands/gsub.rb
@@ -1,6 +1,7 @@
 module Gsub
   def gsub(search, replace, files, for_real=false)
     string = for_real ? "ruby -pi" : "ruby -p"
+    files = "#{files}/**/*.rb" if File.directory?(files)
     string += %[ -e "gsub('#{search}','#{replace}')" #{Dir[files].join(' ')}]
     system(string)
   end
