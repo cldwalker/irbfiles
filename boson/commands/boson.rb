93321b760c4c0644ae8520382b3a64f0f293359f
Gabriel Horner <gabriel.horner@gmail.com>
2009-09-04 08:01:09 -0400

    added help

--- boson/commands/boson.rb
+++ boson/commands/boson.rb
@@ -1,6 +1,18 @@
 module BosonLib
+  def help(name)
+    return "Command not loaded" unless (command = Boson.command(name.to_s) || Boson.command(name.to_s, :alias))
+    return "Library for #{command_obj.name} not found" unless lib = Boson.library(command.lib)
+    return "Can only determine FileLibrary methods for now" unless lib.is_a?(Boson::FileLibrary)
+    lib_string = File.read(Boson::Library.library_file(lib.name))
+    if match = /def\s+#{name}\s*\(?\s*([^\)]+)\s*\)?\s*$/.match(lib_string)
+      "#{name} "+ match.to_a[1].split(/\s*,\s*/).map {|e| "[#{e}]"}.join(' ')
+    else
+      "Command not found in file"
+    end
+  end
+
   def edit_library(name)
-    file = Boson::Library.library_file(name)
+    file = Boson::Library.library_file(name.to_s)
     system("vim", file)
   end
 
