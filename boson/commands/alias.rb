fdcacde74895104201562acf4c19262ea10a67f6
Gabriel Horner <gabriel.horner@gmail.com>
2009-09-08 04:41:43 -0400

    alias config + removed irb config

--- boson/commands/alias.rb
+++ boson/commands/alias.rb
@@ -1,4 +1,9 @@
 module AliasLib
+  def self.config
+    command_aliases = YAML::load_file(File.expand_path("~/.alias.yml"))[:aliases][:instance_method]["Alias::Console"] rescue {}
+    {:command_aliases=>command_aliases}
+  end
+
   def self.included(mod)
     require 'alias'
     eval "module ::MainCommands; end"
