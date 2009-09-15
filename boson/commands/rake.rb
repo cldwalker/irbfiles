d67343e015842931a9d1ccfc4df94211152bbbb8
Gabriel Horner <gabriel.horner@gmail.com>
2009-09-15 04:27:09 -0400

    improved rake

--- boson/commands/rake.rb
+++ boson/commands/rake.rb
@@ -1,9 +1,30 @@
-module Rake
+module RakeLib
   def self.included(mod)
     require 'rake'
   end
 
-  def rake(task)
-    Rake::Task[task.to_s].invoke
+  # options :benchmark=>:boolean
+  # Run rake tasks in current directory. Defaults to test task regardless of rake default
+  def rake(task=nil, options={})
+    ARGV.replace(task ? [task] : ['test'])
+    case ARGV[0]
+    when 'test'
+      ENV['BENCHMARK'] = options[:benchmark] ? 'true' : 'false'
+    end
+    Rake.application.run
+  end
+
+  # Another way of invoking a task
+  def rake_task(task)
+    Rake.application.init
+    Rake.application.load_rakefile
+    Rake.application.invoke_task(task)
+  end
+
+  # Tasks in current directory
+  def rake_tasks
+    Rake.application.init
+    Rake.application.load_rakefile
+    Rake.application.tasks
   end
 end
