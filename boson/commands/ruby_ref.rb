752a29c0b21b9dc41256d187fdd90a212b0aa49f
Gabriel Horner <gabriel.horner@gmail.com>
2009-09-22 06:59:17 -0400

    nice method_missing loader + to_s_id

--- boson/commands/ruby_ref.rb
+++ boson/commands/ruby_ref.rb
@@ -38,6 +38,12 @@ module RubyRef
     render obj.instance_variables.map {|e| [e, obj.instance_variable_get(e).inspect] }
   end
 
+  # From http://solutious.com/blog/2009/09/22/secret-of-object-to_s/
+  # Calculates id found in :to_s of most objects
+  def to_s_id(obj)
+    "0x%x" % [obj.object_id*2]
+  end
+
   private
   def get_loaded_paths
     hash = {}
--- irbrc.rb
+++ irbrc.rb
@@ -11,4 +11,17 @@ require 'rubygems'
   end
 end
 
+# autoload libraries of top level commands
+class << Boson.main_object
+  def method_missing(method, *args, &block)
+    Boson::Index.read
+    if lib = Boson::Index.find_library(method.to_s, nil)
+      Boson::Library.load_library lib, :verbose=>true
+      send(method, *args, &block) if respond_to?(method)
+    else
+      super
+    end
+  end
+end
+
 Boson.start :verbose=>true
