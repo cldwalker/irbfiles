698175133bf0986bd5bff557a1fe04012466129d
Gabriel Horner <gabriel.horner@gmail.com>
2009-09-29 22:16:03 -0400

    more github apis, whoop

--- boson/commands/site/github.rb
+++ boson/commands/site/github.rb
@@ -4,16 +4,16 @@ module Github
   # @options :user=>'cldwalker', :fork_included=>true
   # Displays a user's repositories
   def user_repos(options={})
-    repos = yaml_get("http://github.com/api/v2/yaml/repos/show/#{options[:user]}")['repositories']
-    !options[:fork_included] ? repos.select {|e| ! e['fork'] } : repos
+    repos = github_get("/repos/show/#{options[:user]}")['repositories']
+    !options[:fork_included] ? repos.select {|e| ! e[:fork] } : repos
   end
 
   # @render_options :fields=>{:values=>["score", "name", "size", "language", "followers", "type",
   #   "username", "id", "description", "forks", "fork", "pushed", "created"], :default=>['name','username',
   #   'followers','language','pushed','score','description']}, :sort=>'score', :reverse_sort=>true
   #  Search repositories
-  def repo_search(query, options={})
-    yaml_get("http://github.com/api/v2/yaml/repos/search/#{query}")['repositories']
+  def repo_search(query)
+    github_get("/repos/search/#{query}")['repositories']
   end
 
   # @render_options :fields=>{:values=>[:homepage, :name, :watchers, :private, :forks, :fork, :url, :description, :owner, :open_issues],
@@ -23,7 +23,38 @@ module Github
   def repo_network(user_repo, options={})
     user_repo = convert_user_repo(user_repo)
     user_repo = "#{options[:user]}/#{user_repo}" unless user_repo['/']
-    yaml_get("http://github.com/api/v2/yaml/repos/show/#{user_repo}/network")['network']
+    github_get("/repos/show/#{user_repo}/network")['network']
+  end
+
+  # @render_options :fields=>{:values=>["score", "name", "language", "followers", "type", "fullname",
+  #  "username", "id", "repos", "pushed", "created", "location"], :default=>['name', 'followers', 'repos',
+  #  'pushed', 'language', 'location', 'score']}
+  # Search users
+  def user_search(query)
+    github_get("/user/search/#{query}")['users']
+  end
+
+  # td: /commits/list/:user_id/:repository/:branch/*path
+  # @render_options :fields=>{:values=>%w{id url committed_date authored_date message},
+  #  :default=>%w{id authored_date message}}
+  # @options :branch=>'master'
+  # List commits of a given user-repo
+  def commit_list(user_repo, options={})
+    user_repo = convert_user_repo(user_repo)
+    github_get("/commits/list/#{user_repo}/#{options[:branch]}")['commits']
+  end
+
+  # @render_options :fields=>{:values=>[:owner, :homepage, :open_issues, :name, :url, :private,
+  #  :fork, :watchers, :description, :forks], :default=>[:name, :owner, :watchers, :forks, :description]}
+  # Lists repos watched by user
+  def repos_watched(user)
+    github_get("/repos/watched/#{user}")['repositories'].select {|e| e[:owner] != user }
+  end
+
+  # @render_options {}
+  # List users a user follows
+  def user_follows(user)
+    github_get("/user/show/#{user}/following")['users']
   end
 
   # sample url: http://github.com/takai/twitty-console
@@ -55,6 +86,10 @@ module Github
   private
   def convert_user_repo(user_repo)
     user_repo.include?('-') ? user_repo.split('-', 2).join('/') : user_repo
+  end
+
+  def github_get(url)
+    yaml_get("http://github.com/api/v2/yaml#{url}")
   end
 
   def yaml_get(url)
