48c9b7965e3cb814b79792e7047a5f6c8d749fd8
Gabriel Horner <gabriel.horner@gmail.com>
2009-10-10 04:34:32 -0400

    updated for latest boson

--- boson/commands/tree.rb
+++ boson/commands/tree.rb
@@ -1,21 +1,22 @@
 module Tree
-  OPTIONS = {:as=>:string, :children_method=>:string, :type=>{:type=>:string, :values=>[:directory, :basic, :number]}}
+  OPTIONS = {:class=>:string, :children_method=>:string, :type=>{:type=>:string, :values=>[:directory, :basic, :number]}}
   # @options OPTIONS
   # Prints an inheritance tree given a root class
   def inheritance_tree(klass, options={})
-    render klass, {:as=>:parent_child_tree, :children_method=>:class_children, :type=>:directory}.merge(options)
+    render klass, {:class=>:parent_child_tree, :children_method=>:class_children, :type=>:directory}.merge(options)
   end
 
   # @options OPTIONS
   # Prints an inheritance tree without Exception classes
   def errorless_inheritance_tree(klass, options={})
     child_lambda = lambda {|n| n.class_children.reject {|e| e < Exception || e.name =~ /Errno/} }
-    render klass, {:as=>:parent_child_tree, :children_method=>child_lambda, :type=>:directory}.merge(options)
+    render klass, {:class=>:parent_child_tree, :children_method=>child_lambda, :type=>:directory}.merge(options)
   end
 
   # @options OPTIONS
   # Prints a tree of nested classes under a given class/module.
   def nested_tree(klass, options={})
-    render klass, {:as=>:parent_child_tree, :children_method=>:nested_children, :value_method=>:nested_name, :type=>:directory}.merge(options)
+    render klass, {:class=>:parent_child_tree, :children_method=>:nested_children, :value_method=>:nested_name,
+      :type=>:directory}.merge(options)
   end
 end
