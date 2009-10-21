7561fb41222c68e390eb66cdea771d24b3c67715
Gabriel Horner <gabriel.horner@gmail.com>
2009-10-20 21:50:55 -0400

    isitruby19 tweaks

--- boson/commands/site/is_it_ruby19.rb
+++ boson/commands/site/is_it_ruby19.rb
@@ -1,27 +1,26 @@
 # API ref: http://forum.brightbox.co.uk/forums/isitruby19-com/topics/api-is-added
 module IsItRuby19
-  def r19_gem(name)
-    render r19_formatted_comments(name), :fields=>%w{version works_for_me platform body name url}
+  # @render_options :fields=>{:values=>%w{version works_for_me platform body name url}}
+  def gem(name)
+    comment_feed(name).each {|e| e["platform"] = e['platform']['name']; e["body"] = e["body"].gsub(/\r?\n\r?/,";") }
   end
 
-  def r19_stat(name)
-    comments = r19_comments(name)
+  # @render_options :fields=>[:name, :stat]
+  def stats(*names)
+    names.map {|e| stat(e) }
+  end
+
+  private
+  def stat(name)
+    comments = comment_feed(name)
     worked = comments.select {|e| e["works_for_me"] }.size
     stat = comments.size.zero? ? "0" : "#{worked}/#{comments.size} (#{sprintf('%.2f', worked / comments.size.to_f)})"
     {:name=>name, :stat=>stat}
   end
 
-  def r19_stats(names)
-    names.map {|e| r19_stat(e) }
-  end
-
-  def r19_comments(name)
+  def comment_feed(name)
     get("http://isitruby19.com/#{name}/comments.json").map {|e| e["comment"]}
   rescue
     []
-  end
-
-  def r19_formatted_comments(name)
-    r19_comments(name).each {|e| e["platform"] = e['platform']['name']; e["body"] = e["body"].gsub(/\r?\n\r?/,";") }
   end
 end
