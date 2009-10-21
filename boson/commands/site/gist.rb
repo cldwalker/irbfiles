09fb1cefe8750396ad87dc96dfa93d73fc16cdbe
Gabriel Horner <gabriel.horner@gmail.com>
2009-10-21 15:47:41 -0400

    removing tabs copied from original gist

--- boson/commands/site/gist.rb
+++ boson/commands/site/gist.rb
@@ -18,38 +18,38 @@ module Gist
     end
   end
 
-	class << self
-		GIST_URL = 'http://gist.github.com/%s.txt'
-		@proxy = ENV['http_proxy'] ? URI(ENV['http_proxy']) : nil
+  class << self
+    GIST_URL = 'http://gist.github.com/%s.txt'
+    @proxy = ENV['http_proxy'] ? URI(ENV['http_proxy']) : nil
 
-		def read(gist_id)
-			return open(GIST_URL % gist_id).read unless gist_id.to_i.zero?
-			return open(gist_id + '.txt').read if gist_id[/https?:\/\/gist.github.com\/\d+$/]
-		end
+    def read(gist_id)
+      return open(GIST_URL % gist_id).read unless gist_id.to_i.zero?
+      return open(gist_id + '.txt').read if gist_id[/https?:\/\/gist.github.com\/\d+$/]
+    end
 
-		def write(content, options)
-			url = URI.parse('http://gist.github.com/gists')
-			if @proxy
-				req = Net::HTTP::Proxy(@proxy.host, @proxy.port).post_form(url, data(nil, nil, content, options[:private]))
-			else
-				req = Net::HTTP.post_form(url, data(options[:file], nil, content, options[:private]))
-			end
-			req['Location']
-		end
+    def write(content, options)
+      url = URI.parse('http://gist.github.com/gists')
+      if @proxy
+        req = Net::HTTP::Proxy(@proxy.host, @proxy.port).post_form(url, data(nil, nil, content, options[:private]))
+      else
+        req = Net::HTTP.post_form(url, data(options[:file], nil, content, options[:private]))
+      end
+      req['Location']
+    end
 
     private
-		def data(name, ext, content, private_gist)
-			return {
-				'file_ext[gistfile1]'      => ext,
-				'file_name[gistfile1]'     => name,
-				'file_contents[gistfile1]' => content
-			}.merge(private_gist ? { 'action_button' => 'private' } : {}).merge(auth)
-		end
+    def data(name, ext, content, private_gist)
+      return {
+        'file_ext[gistfile1]'      => ext,
+        'file_name[gistfile1]'     => name,
+        'file_contents[gistfile1]' => content
+      }.merge(private_gist ? { 'action_button' => 'private' } : {}).merge(auth)
+    end
 
-		def auth
-			user  = `git config --global github.user`.strip
-			token = `git config --global github.token`.strip
-			user.empty? ? {} : { :login => user, :token => token }
-		end
-	end
+    def auth
+      user  = `git config --global github.user`.strip
+      token = `git config --global github.token`.strip
+      user.empty? ? {} : { :login => user, :token => token }
+    end
+  end
 end
