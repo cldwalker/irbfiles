d5f9058328ce4a313985cbf51c36f09b791add09
Gabriel Horner <gabriel.horner@gmail.com>
2009-11-07 07:11:52 -0500

    ruby ref tweaks

--- boson/commands/public/ruby_ref.rb
+++ boson/commands/public/ruby_ref.rb
@@ -1,52 +1,48 @@
 # Gives useful info about current environment
 module RubyRef
-  # @render_options :headers=>{:default=>{0=>"variable",1=>"value"}}
+  # @render_options :change_fields=>{:default=>{0=>"variable",1=>"value"}}
   # Prints global variables and their values
   def global_var
     global_variables.sort.map {|e| [e, (eval e).inspect] }
   end
 
-  # @render_options :fields=>[:require_path, :full_path], :sort=>'require_path'
-  # @options :reload=>:boolean
+  # @render_options :change_fields=>{:default=>{0=>'require_path', 1=>'full_path'}}, :sort=>'require_path'
   # Prints loaded paths and their full paths
-  def loaded_paths(options={})
-    @loaded_paths = RubyRef.get_loaded_paths if options[:reload] || @loaded_paths.nil?
-    @loaded_paths.inject([]) {|t,(k,v)| t << {:require_path=>k, :full_path=>v } }
+  def loaded_paths
+    hash = {}
+    $".each {|file|
+      $:.each {|dir|
+        if test(?e, File.join(dir, file))
+          hash[file] = File.join(dir, file)
+          break
+        end
+      }
+    }
+    hash
   end
 
-  # @render_options :headers=>{:default=>{0=>"name", 1=>"value"}}
+  # @render_options :change_fields=>{:alias=>false, :default=>{0=>"name", 1=>"value"}}
   # Rbconfig constants and their values
   def rbconfig
     require 'rbconfig'
     RbConfig::CONFIG
   end
 
-  # @render_options {}
+  # @render_options :change_fields=>{:default=>{0=>'instance', 1=>'value'}},
+  #   :filters=>{:default=>{'value'=>:inspect}}
   # Table of an object's instance variables
-  def instance_var_table(obj)
-    obj.instance_variables.map {|e| [e, obj.instance_variable_get(e).inspect] }
+  def instance_var(obj)
+    obj.instance_variables.map {|e| [e, obj.instance_variable_get(e)] }
   end
 
-  # @render_options :headers=>{:default=>{0=>'name', 1=>'version'}}
+  # @render_options :change_fields=>{:default=>{0=>'name', 1=>'version'}}
   # @options :loaded_path=>:boolean
   # List versions of currently loaded gems
-  def version_list(options)
-    options[:loaded_path] ? $:.map {|e| e =~ /\/([\w-]+)-(\d\.\d(\.\d)?)\/lib/ ?
-      [$1,$2] : nil }.compact.uniq :
-      Gem.loaded_specs.values.map {|e| [e.name, e.version.to_s] }
-  end
-
-  def self.get_loaded_paths
-    hash = {}
-    $".each { |f|
-      $:.each { |d|
-        test_file = File.join(d, f)
-        if test(?e,test_file)
-          hash[f] = test_file
-          break
-        end
-      }
-    }
-    hash
+  def gem_versions(options={})
+    if options[:loaded_path]
+      $:.map {|e| e =~ /\/([\w-]+)-(\d\.\d(\.\d)?)\/lib/ ? [$1,$2] : nil }.compact.uniq
+    else
+      Gem.loaded_specs.values.map {|e| [e.name, e.version] }
+    end
   end
 end
