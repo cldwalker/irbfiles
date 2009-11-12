8f3533438ab00532180fc7be6438e022a55536f8
Gabriel Horner <gabriel.horner@gmail.com>
2009-11-12 04:51:28 -0500

    gemcutter cmds

---
+++ boson/commands/public/site/gemcutter.rb
@@ -0,0 +1,17 @@
+module Gemcutter
+  def self.included(mod)
+    require 'httparty'
+  end
+
+  # @render_options :change_fields=>{:default=>{0=>'field', 1=>'value'}}
+  # Lists basic gemcutter stats for gem
+  def cut(gem_name)
+    HTTParty.get("http://gemcutter.org/gems/#{gem_name}.json")
+  end
+
+  # @render_options :fields=>["name", "downloads", "info", "version", "authors", "rubyforge_project"]
+  # Lists multiple gemcutter gems + stats
+  def cuts(*gems)
+    gems.inject([]) {|t,e| t << HTTParty.get("http://gemcutter.org/gems/#{e}.json") }
+  end
+end
