60f240431a83a35e24ca900a61ba02303f856b6e
Gabriel Horner <gabriel.horner@gmail.com>
2009-11-15 04:35:48 -0500

    basic code stats

---
+++ boson/commands/public/code_stats.rb
@@ -0,0 +1,16 @@
+module CodeStats
+  # @render_options :change_fields=>%w{field value}
+  # Displays code stats for a ruby directory: method count, loc, class count
+  def code_stats(dir='lib')
+    gem 'rails'
+    require 'code_statistics'
+    CodeStatistics.new([dir]).instance_variable_get("@statistics")[dir]
+  end
+
+  # @config :alias=>'loc'
+  # @render_options :change_fields=>%w{file loc}
+  # Line count per file for ruby files
+  def lines_of_code(dir='lib')
+    Dir.glob(dir+"/**/*.rb").map {|e| [e, File.readlines(e).size ] }
+  end
+end
