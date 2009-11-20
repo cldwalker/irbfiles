d315198d7bff25b96282da7ec12054c7c39daf7d
Gabriel Horner <gabriel.horner@gmail.com>
2009-11-20 07:16:16 -0500

    tweaking gmail to read inbox w/ ruby-gmail

--- boson/commands/public/site/gmail.rb
+++ boson/commands/public/site/gmail.rb
@@ -1,17 +1,24 @@
 # for less dependencies could use http://gist.github.com/122071
-module Gmail
+module GmailLib
   def self.included(mod)
-    require 'gmail_sender'
+    # require 'gmail_sender'
+    require 'gmail' #ruby-gmail gem
+  end
+
+  #@render_options :fields=>[:uid, :body], :filters=>{:default=>{:body=>[:[], /Subject: (.*)\r\n/, 1]}}
+  #@options :unread=>true
+  def inbox(options={})
+    options[:unread] ? gmail.inbox.emails(:unread) : gmail.inbox.emails
   end
 
   # Send email from your gmail acount. ENV['GOOGLE_USER'] and ENV['GOOGLE_PASSWORD']
   # should be setup.
-  def email(to, subject, body)
-    gmail.send(to, subject, body)
-  end
+  # def email(to, subject, body)
+  #   gmail.send(to, subject, body)
+  # end
 
   private
   def gmail
-    @gmail ||= GmailSender.new(ENV['GOOGLE_USER'], ENV['GOOGLE_PASSWORD'])
+    @gmail ||= Gmail.new(ENV['GOOGLE_USER'], ENV['GOOGLE_PASSWORD'])
   end
 end
