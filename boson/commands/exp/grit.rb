dd845343d230dc1479206e0e3d3ef5fd87cb40d2
Gabriel Horner <gabriel.horner@gmail.com>
2009-11-23 13:44:45 -0500

    more git cmds

--- boson/commands/exp/grit.rb
+++ boson/commands/exp/grit.rb
@@ -3,7 +3,44 @@ module Git
     require 'grit'
   end
 
+  # @render_options :change_fields=>['repo', 'pending'], :sort=>'pending',
+  #  :reverse_sort=>true
+  # Lists # of commits that haven't been pushed to origin/master
+  def pending_commits
+    Git.repos.map{|e|
+      [File.basename(e.working_dir), e.log('origin/master..master').size]
+    }
+  end
+
+  # List repo's latest commits
+  def repo_commits(repo)
+    Git.repo_hash[repo].commits
+  end
+
+  # @render_options :change_fields=>['repo', 'changed', 'added', 'deleted'], :sort=>'changed',
+  #  :reverse_sort=>true
+  # List status of repos
+  def repo_stati
+    Git.repos.map {|e|
+      status = e.status
+      [File.basename(e.working_dir), status.changed.size, status.added.size, status.deleted.size]
+    }
+  end
+
+  # Push given repos to origin/master
+  def push_repos(*repos)
+    repos.each do |e|
+      Git.repo_hash[e].git.push
+    end
+  end
+
+  # Hash to access repo objects
+  def repos
+    Git.repo_hash
+  end
+
   # @render_options :change_fields=>['repo', 'date']
+  # Lists last commit of each repo
   def last_commits
     Git.repos.map {|e|
       [File.basename(e.working_dir), e.commits[0].committed_date]
@@ -13,7 +50,7 @@ module Git
   class<<self
     def repo_hash
       @repo_hash ||= begin
-        dirs.inject({}) {|a,e| a[e] = Grit::Repo.new(e); a }
+        dirs.inject({}) {|a,e| a[File.basename(e)] = Grit::Repo.new(e); a }
       end
     end
 
