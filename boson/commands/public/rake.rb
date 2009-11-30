3d034f6c3a7d1fa82633efe28daf03d620f56791
Gabriel Horner <gabriel.horner@gmail.com>
2009-11-30 14:49:39 -0500

    rake cmd tweaks

--- boson/commands/public/rake.rb
+++ boson/commands/public/rake.rb
@@ -3,28 +3,43 @@ module RakeLib
     require 'rake'
   end
 
-  # @options :benchmark=>:boolean
-  # Run rake tasks in current directory. Defaults to test task regardless of rake default
-  def rake(task=nil, options={})
-    ARGV.replace(task ? [task] : ['test'])
-    case ARGV[0]
-    when 'test'
-      ENV['BENCHMARK'] = options[:benchmark] ? 'true' : 'false'
+  # @options :benchmark=>:boolean, :run=>:boolean
+  # Execute a rake task
+  def rake(task='default', options={})
+    clean_argv
+    ENV['BENCHMARK'] = options[:benchmark] ? 'true' : 'false' # to enable benchmark gem
+    if options[:run]
+      ARGV.replace [task]
+      Rake.application.run
+    else
+      unless @rake_tasks_loaded
+        Rake.application.init
+        Rake.application.load_rakefile
+      end
+      Rake.application.invoke_task task
     end
-    Rake.application.run
   end
 
-  # Another way of invoking a task
-  def rake_task(task)
-    Rake.application.init
-    Rake.application.load_rakefile
-    Rake.application.invoke_task(task)
+  # @render_options :fields=>[:name, :comment]
+  # @options :reload=>{:type=>:boolean, :desc=>"Clears existing tasks and reloads current directory tasks"},
+  #  :add=>{:type=>:boolean, :desc=>'Adds tasks from current directory'},
+  #  :all=>{:type=>:boolean, :desc=>'Lists all tasks including task dependencies'}
+  # Lists loaded tasks
+  def rake_tasks(options={})
+    clean_argv
+    if options[:reload] || @rake_tasks_loaded.nil?
+      Rake.application.clear
+      Rake.application.init
+      Rake.application.load_rakefile
+      @rake_tasks_loaded = true
+    elsif options[:add]
+      Rake.application.load_rakefile
+    end
+    options[:all] ? Rake.application.tasks : Rake.application.tasks.select {|e| e.comment}
   end
 
-  # Tasks in current directory
-  def rake_tasks
-    Rake.application.init
-    Rake.application.load_rakefile
-    Rake.application.tasks
+  private
+  def clean_argv
+    ARGV.delete_if {|e| e[/^-/] } unless ARGV.empty?
   end
 end
