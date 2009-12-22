a208b69c04a765c3a763cdcff989452761552df0
Gabriel Horner <gabriel.horner@gmail.com>
2009-12-22 11:35:43 -0500

    gemcutter api tweaks

--- boson/commands/public/site/gemcutter.rb
+++ boson/commands/public/site/gemcutter.rb
@@ -6,12 +6,12 @@ module Gemcutter
   # @render_options :change_fields=>%w{field value}
   # Lists basic gemcutter stats for gem
   def cut(gem_name)
-    HTTParty.get("http://gemcutter.org/gems/#{gem_name}.json")
+    HTTParty.get("http://gemcutter.org/api/v1/gems/#{gem_name}.json")
   end
 
   # @render_options :fields=>["name", "downloads", "info", "version", "authors", "rubyforge_project"]
   # Lists multiple gemcutter gems + stats
   def cuts(*gems)
-    gems.inject([]) {|t,e| t << HTTParty.get("http://gemcutter.org/gems/#{e}.json") }
+    gems.inject([]) {|t,e| t << HTTParty.get("http://gemcutter.org/api/v1/gems/#{e}.json") }
   end
 end
