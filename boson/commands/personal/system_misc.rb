465d976767a99edf8609cdeb21f1fd4fc84bf63e
Gabriel Horner <gabriel.horner@gmail.com>
2010-01-03 01:15:46 -0500

    happy new year

--- boson/commands/personal/system_misc.rb
+++ boson/commands/personal/system_misc.rb
@@ -32,7 +32,7 @@ module SystemMisc
 
   # Opens google analytics for given date range
   def analytics_day(start_date=nil, end_date=nil)
-    start_date = start_date ? Date.parse("#{start_date}/2009") : Date.today
+    start_date = start_date ? Date.parse("#{start_date}/2010") : Date.today
     start_date = start_date.strftime("%Y%m%d")
     end_date ||= start_date
     url = "https://www.google.com/analytics/reporting/?reset=1&id=14680769&pdr=#{start_date}-#{end_date}"
