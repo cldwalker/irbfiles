7d083fcb8718d6c7de9e3ba2ce7dfb814130b04e
Gabriel Horner <gabriel.horner@gmail.com>
2010-01-08 05:50:58 -0500

    classified + improved menu plugin

--- boson/commands/exp/plugins/menu.rb
+++ boson/commands/exp/plugins/menu.rb
@@ -5,40 +5,72 @@ module ::Boson::Scientist
       filters = @global_options.delete(:filters)
       table = ::Hirb::Helpers::Table.new(result, @global_options)
       result = table.instance_eval("@rows")
-      menu(result, menu_options.merge(:filters=>filters))
+      Menu.run(result, menu_options.merge(:filters=>filters), @global_options)
+      nil
     else
       # @global_options[:render] = true
       _render_or_raw(result)
     end
-    nil
   end
 
-  # options :loop, :execute_at_once, :transform_functions
-  def menu(items, options={})
-    fields = @global_options[:fields] ? @global_options[:fields] :
-      @global_options[:change_fields] ? @global_options[:change_fields] :
-      items[0].is_a?(Hash) ? items[0].keys : [:to_s]
-    options[:default_field] = options[:default_field] ?
-      (fields.map {|e| e.to_s }.sort.find {|e| e[/^#{options[:default_field]}/] } ||
-        options[:default_field]) : fields[0]
+  class Menu
+    require 'shellwords'
+    def self.run(items, options, global_options)
+      new(items, options, global_options).run
+    end
 
-    # p options
-    input = Boson.invoke(:menu, items, :return_input=>true, :fields=>fields)
-    args = parse_input(items, input)
-    execute_command(args, options)
-  end
+    def initialize(items, options, global_options)
+      @items, @options, @global_options = items, options, global_options
+      @fields = @global_options[:fields] ? @global_options[:fields] :
+        @global_options[:change_fields] ? @global_options[:change_fields] :
+        items[0].is_a?(Hash) ? items[0].keys : [:to_s]
+      @options[:default_field] = @options[:default_field] ?
+        unalias_field(@options[:default_field]) : @fields[0]
+    end
 
-  def execute_command(args, options)
-    cmd = args.shift
-    args.flatten.each {|e|
-      Boson.full_invoke cmd, [e[options[:default_field]]]
-    }
-  end
+    def run
+      input = get_input
+      if @options[:shell]
+        while input != 'q'
+          parse_and_invoke input
+          input = get_input
+        end
+      else
+        parse_and_invoke input
+      end
+    end
 
-  def parse_input(items, input)
-    input.split(/\s+/).map {|e|
-      e[/^\d+/] ? ::Hirb::Util.choose_from_array(items, e) : e
-    }
+    def get_input
+      ::Boson.invoke(:menu, @items, :return_input=>true, :fields=>@fields)
+    end
+
+    def unalias_field(field)
+      @fields.sort_by {|e| e.to_s }.find {|e| e.to_s[/^#{field}/] } || field
+    end
+
+    def parse_and_invoke(input)
+      cmd, *args = parse_input(input)
+      @options[:once] ? invoke(cmd, args.flatten) : args.flatten.each {|e| invoke(cmd, [e]) }
+    end
+
+    def invoke(cmd, args)
+      if @options[:pretend]
+        puts "#{cmd} #{args.inspect}"
+      else
+        ::Boson.full_invoke cmd, args
+      end
+    end
+
+    def parse_input(input)
+      Shellwords.shellwords(input).map {|word|
+        if word[/^(\d(?:[^:]+)?)(?::)?(\S+)?/]
+          field = $2 ? unalias_field($2) : @options[:default_field]
+          ::Hirb::Util.choose_from_array(@items, $1).map {|e| e[field] }
+        else
+          word
+        end
+      }
+    end
   end
 end
 
