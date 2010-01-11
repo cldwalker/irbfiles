8609c15850eb18a15241b0310f138fa98ff2db88
Gabriel Horner <gabriel.horner@gmail.com>
2010-01-10 19:11:16 -0500

    tweaked all option commands plugin

--- boson/commands/public/plugins/all_option_commands.rb
+++ boson/commands/public/plugins/all_option_commands.rb
@@ -1,12 +1,19 @@
 # This plugin forces all commands to be wrapped by an OptionCommand.
 # This is useful when wanting to use the option_command_filters plugin on non-option commands.
+# This plugins only affects commands from commandline.
 module AllOptionCommands
+  def self.append_features(mod)
+    super if Boson.const_defined?(:BinRunner)
+  end
+
   def self.after_included
     ::Boson::Command.module_eval do
       class <<self
         alias_method :_new_options, :new_options
         def new_options(name, library)
-          _new_options(name, library).merge!(:global_options=>true)
+          opts = _new_options(name, library)
+          !opts.key?(:render_options) && !opts.key?(:options) ?
+            opts.merge!(:global_options=>true) : opts
         end
       end
     end
