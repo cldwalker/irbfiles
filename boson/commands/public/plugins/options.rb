46b622f6ae38e515d285cb848a641323a3fa34fb
Gabriel Horner <gabriel.horner@gmail.com>
2010-01-24 19:23:02 -0500

    hash option can have boolean values

--- boson/commands/public/plugins/options.rb
+++ boson/commands/public/plugins/options.rb
@@ -1,3 +1,22 @@
+class ::Boson::OptionParser
+  # Allows for boolean values in hashes
+  def parse_hash(value, keys)
+    if value[/:$/]
+      splitter = current_attributes[:split] || ','
+      value.gsub(/:$/, '').split(/#{Regexp.quote(splitter)}/).inject({}) {|t,e|
+        key = keys ? auto_alias_value(keys, e) : e
+        t[key] = true; t
+      }
+    else
+      super
+    end
+  end
+end
+
+# it "with value ending in ':' assumes true value" do
+#   parse('-e', 'tw,o:')[:e].should == {:two=>true, :one=>true}
+# end
+
 module ::Boson::Options::Date
   def create_date(value)
    Date.parse(value + "/#{Date.today.year}")
