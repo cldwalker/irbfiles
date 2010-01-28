160a80ddfc5333c1cb0dcaa582f82c49cca5b9ba
Gabriel Horner <gabriel.horner@gmail.com>
2010-01-28 14:09:53 -0500

    handy gem_tree

--- boson/commands/personal/tree.rb
+++ boson/commands/personal/tree.rb
@@ -25,4 +25,23 @@ module Tree
     render klass, {:class=>:parent_child_tree, :children_method=>lambda {|e| nested_children(e) },
      :value_method=>lambda {|e| nested_name(e)}, :type=>:directory}.merge(options)
   end
+
+  # @options OPTIONS
+  # Displays a nested_tree for the most likely top-level module representing a gem i.e. nokogiri -> Nokogiri
+  def gem_tree(rubygem, options={})
+    modules = ::Boson::Util.detect(:modules=>true) { require rubygem }[:modules]
+    if (klass = ::Boson::Util.constantize(rubygem))
+      nested_tree(klass, options)
+    elsif (klasses = modules.select {|e| e.to_s[/^[^:]+$/] })
+      if (possible_klasses = klasses.select {|e| e.to_s[/^#{rubygem}/i] }).size == 1
+        nested_tree(possible_klasses[0], options)
+      else
+        # Just display the first one we know of
+        nested_tree(klasses[0], options)
+        puts "Multiple base modules were detected: #{klasses.map {|e| e.to_s }.join(', ')}"
+      end
+    else
+      puts "No base module found for '#{rubygem}'"
+    end
+  end
 end
