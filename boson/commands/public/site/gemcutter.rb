142d181525ea1155e13985ad39954ccff98cb1a4
Gabriel Horner <gabriel.horner@gmail.com>
2010-01-31 22:10:11 -0500

    cutter search

--- boson/commands/public/site/gemcutter.rb
+++ boson/commands/public/site/gemcutter.rb
@@ -17,4 +17,13 @@ module Gemcutter
       response.is_a?(Hash) ? t << response : t
     }
   end
+
+  # @render_options :fields=>{:values=>%w{name authors version_downloads info project_uri gem_uri version downloads},
+  #  :default=>%w{name downloads project_uri info}}
+  # @options :page=>1
+  # @config :alias=>'cs'
+  # Searches gemcutter
+  def cut_search(query, options={})
+    HTTParty.get "http://gemcutter.org/api/v1/search.json?page=#{options[:page]}&query=#{query}"
+  end
 end
