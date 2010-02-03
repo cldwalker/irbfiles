bd0166e1e1369c5b67c05cf5b4475685255e3804
Gabriel Horner <gabriel.horner@gmail.com>
2010-02-02 20:03:50 -0500

    gemcutter tweaks

--- boson/commands/public/site/gemcutter.rb
+++ boson/commands/public/site/gemcutter.rb
@@ -1,29 +1,34 @@
 module Gemcutter
+  FIELDS = %w{name authors version_downloads info project_uri gem_uri version downloads}
   def self.included(mod)
-    require 'httparty'
+    require 'json'
   end
 
   # @render_options :change_fields=>%w{field value}
   # Lists basic gemcutter stats for gem
   def cut(rubygem)
-    HTTParty.get("http://gemcutter.org/api/v1/gems/#{rubygem}.json")
+    json_get "http://gemcutter.org/api/v1/gems/#{rubygem}.json"
   end
 
-  # @render_options :fields=>["name", "downloads", "info", "version", "authors", "rubyforge_project"]
+  # @render_options :fields=>{:values=>FIELDS, :default=>%w{name downloads project_uri info}}
   # Lists multiple gemcutter gems + stats
   def cuts(*rubygems)
     rubygems.inject([]) {|t,e|
-      response = HTTParty.get("http://gemcutter.org/api/v1/gems/#{e}.json")
+      response = json_get("http://gemcutter.org/api/v1/gems/#{e}.json")
       response.is_a?(Hash) ? t << response : t
     }
   end
 
-  # @render_options :fields=>{:values=>%w{name authors version_downloads info project_uri gem_uri version downloads},
-  #  :default=>%w{name downloads project_uri info}}
+  # @render_options :fields=>{:values=>FIELDS, :default=>%w{name downloads project_uri info}}
   # @options :page=>1
   # @config :alias=>'cs'
   # Searches gemcutter
   def cut_search(query, options={})
-    HTTParty.get "http://gemcutter.org/api/v1/search.json?page=#{options[:page]}&query=#{query}"
+    json_get "http://gemcutter.org/api/v1/search.json?page=#{options[:page]}&query=#{query}"
+  end
+
+  # Gets a json url and converts it to a ruby object
+  def json_get(url)
+    JSON.parse get(url)
   end
 end
