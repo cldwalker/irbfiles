659fd9d0daa1cbbe796ccbd8f147d6c7b904b074
Gabriel Horner <gabriel.horner@gmail.com>
2010-02-12 09:39:36 -0500

    new menu working except for template

--- boson/commands/exp/plugins/menu.rb
+++ boson/commands/exp/plugins/menu.rb
@@ -137,12 +137,82 @@ class ::Menu
   end
 end
 
+class ::TwoDMenu < ::Hirb::Menu
+  OPTIONS = {:default_field=>:string, :pretend=>:boolean, :multiple_execute=>:boolean, :help=>:boolean,
+    [:default_command, :c]=>:string, :splat=>:boolean, :object=>:boolean}
+
+  def self.option_parser
+    @option_parser ||= ::Boson::OptionParser.new OPTIONS
+  end
+
+  def initialize(options={})
+    options = (options[:config] || {}).merge(options[:global_options] || {})
+    super options.merge(:two_d=>true, :readline=>true, :execute=>true)
+  end
+
+  def split_input_args(input)
+    args = Shellwords.shellwords(input)
+    @options = @options.merge self.class.option_parser.parse(args, :opts_before_args=>true)
+    args
+  end
+
+  def return_cell_values?
+    @options[:two_d] && !@options[:object]
+  end
+
+  def execute(items)
+    if @options[:help]
+      self.class.option_parser.print_usage_table
+    else
+      cmd = get_command
+      if @options[:multiple_execute]
+        items.each {|e|
+          args = add_chosen_to_args(e)
+          invoke(cmd, args)
+        }
+      else
+        args = add_chosen_to_args(items)
+        invoke(cmd, args)
+      end
+    end
+    nil
+  end
+
+  def invoke(cmd, args)
+    @options[:splat] = true if ::Boson::Index.read && (cmd_obj = ::Boson::Index.find_command(cmd)) &&
+      cmd_obj.has_splat_args?
+    if @options[:pretend]
+      puts "#{cmd} #{@options[:splat] ? '*' : ''}#{args.inspect}"
+    else
+      output = @options[:splat] ? (::Boson.full_invoke cmd, args.flatten) : ::Boson.full_invoke(cmd, args)
+      unless ::Boson::View.silent_object?(output)
+        opts = output.is_a?(String) ? {:method=>'puts'} : {:inspect=>!output.is_a?(Array) }
+        ::Boson::View.render(output, opts)
+      end
+    end
+  end
+
+end
+
 module MenuLib
   def self.after_included
     ::Boson::Pipe.add_pipes :menu=>{
       :bool_default=>{}, :alias=>['m'], :type=>:hash, :keys=>::Menu::OPTIONS.keys,
       :no_render=>true, :env=>true, :solo=>true, :pipe=>:run_menu, :filter=>true
+    },
+    :zmenu=>{:type=>:boolean,
+      :no_render=>true, :env=>true, :solo=>true, :pipe=>:two_d_menu, :filter=>true
     }
+  end
+
+  def two_d_menu(output, menu_opts)
+    # ::Hirb::Menu.render(output, menu_opts.merge(:execute=>true, :two_d=>true))
+    ::TwoDMenu.render(output, menu_opts)
+  end
+
+  # @render_options :fields=>[:name, :homepage]
+  def gemspecs
+    ::Gem.source_index.gems.values[0,10]
   end
 
   # Runs an awesome menu system on top of hirb's tables
