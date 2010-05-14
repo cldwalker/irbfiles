decdf6581dffc3029f925ed5e848754536c8fa94
Gabriel Horner <gabriel.horner@gmail.com>
2010-05-14 15:59:03 -0400

    github cmd tweaks

--- boson/commands/public/url/github.rb
+++ boson/commands/public/url/github.rb
@@ -18,10 +18,13 @@ module GithubUrl
   end
 
   # @options :user=>{:default=>'cldwalker', :desc=>'Github user'},
-  #  :file=>{:type=>:string, :desc=>'Relative file path within repository' },
-  #  :subpage=>{:type=>:string, :values=>%w{readme wiki issues network commits traffic punch_card timeline},
-  #    :desc=>'Subpage belonging to repo', :bool_default=>'readme' }
-  # Opens a repo with an optional path in a browser
+  #  :file=>{:type=>:string, :desc=>'Relative file path within repository'},
+  #  :commit=>{:type=>:string, :desc=>'Commit page'},
+  #  :issue=>{:type=>:string, :desc=>'Issue page'},
+  #  :tree=>{:type=>:string, :desc=>'Tree page'},
+  #  :subpage=>{:type=>:string, :values=>%w{readme wiki issues network commits traffic punch_card timeline edit branches},
+  #    :enum=>false, :desc=>'Subpage belonging to repo', :bool_default=>'readme' }
+  # Opens a repo page or a subpage i.e. commit, tree, file in a browser
   def repo(user_repo, options={})
     user_repo = "#{options[:user]}/#{user_repo}" unless user_repo['/']
     repo_url = "http://github.com/#{user_repo}"
@@ -29,17 +32,28 @@ module GithubUrl
       repo_url << "/blob/master/" + options[:file]
     elsif options[:subpage]
       case options[:subpage]
-      when 'wiki'    then repo_url.sub!('github.com', 'wiki.github.com')
-      when 'issues'  then repo_url << '/issues'
-      when 'network' then repo_url << '/network'
-      when 'commits' then repo_url << '/commits'
-      when 'traffic' then repo_url << '/graphs/traffic'
-      when 'punch_card' then repo_url << '/graphs/punch_card'
+      when 'wiki'     then repo_url.sub!('github.com', 'wiki.github.com')
+      when 'traffic'  then repo_url << '/graphs/traffic'
+      when 'readme'   then repo_url << '#readme'
       when 'timeline' then repo_url << '/graphs/impact'
-      else
-        repo_url << '#readme'
+      when 'punch_card' then repo_url << '/graphs/punch_card'
+      else repo_url << "/#{options[:subpage]}"
       end
+    elsif options[:commit]
+      repo_url << "/commit/#{options[:commit]}"
+    elsif options[:tree]
+      repo_url << "/tree/#{options[:tree]}"
+    elsif options[:issue]
+      repo_url << "/issues#issue/#{options[:issue]}"
     end
     repo_url
+  end
+
+  # @options :user=>{:default=>'cldwalker', :desc=>'Github user'},
+  #  :start=>'master', :end=>'master'
+  # Compare repos by branch or commit
+  def repo_compare(user_repo, options={})
+    user_repo = "#{options[:user]}/#{user_repo}" unless user_repo['/']
+    "http://github.com/#{user_repo}/compare/#{options[:start]}...#{options[:end]}"
   end
 end
