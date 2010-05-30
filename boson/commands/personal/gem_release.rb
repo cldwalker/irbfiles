f3f25911cfdc6bd85b4f15eaf8f951c66389a2e0
Gabriel Horner <gabriel.horner@gmail.com>
2010-05-30 11:34:56 -0400

    fixed + tweaked bump cmd

--- boson/commands/personal/gem_release.rb
+++ boson/commands/personal/gem_release.rb
@@ -4,21 +4,26 @@ module GemRelease
   end
 
   # @options :file=>:string, :bump_type=>{:default=>'patch', :values=>['major','minor','patch']}
-  # Bumps version in a version file
+  # Bumps version in a version file and checks in to git
   def bump(options={})
+    new_version = bump_file(options)
+    system "git commit -am 'Bumped to version #{new_version}'"
+  end
+
+  def bump_file(options)
     version_file = options[:file] || Dir['**/version.rb'][0] || raise("No version file found")
     version_string = File.read(version_file)
     new_version = nil
     version_string.sub!(/(\d+)\.(\d+)\.(\d+)/) {|e|
       major, minor, patch = $1.to_i, $2.to_i, $3.to_i
       new_version = case options[:bump_type]
-      when 'major' then "#{major+1}.#{minor}.#{patch}"
-      when 'minor' then "#{major}.#{minor+1}.#{patch}"
+      when 'major' then "#{major+1}.0.0"
+      when 'minor' then "#{major}.#{minor+1}.0"
       else              "#{major}.#{minor}.#{patch+1}"
       end
     }
     File.open(version_file, 'w') {|f| f.write version_string }
-    "Updated '#{version_file}' to '#{new_version}'"
+    new_version
   end
 
   # Tags release with current version
