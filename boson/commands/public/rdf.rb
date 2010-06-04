5702e2bb62718faa88e5bc007bd596215ffb0025
Gabriel Horner <gabriel.horner@gmail.com>
2010-06-04 00:06:09 -0400

    more endpoints, added sparql filters

--- boson/commands/public/rdf.rb
+++ boson/commands/public/rdf.rb
@@ -13,11 +13,24 @@ module RdfLib
     RDF::Graph.load(uri).data.map {|e| e.to_a }
   end
 
-  ENDPOINTS = {'bio'=>'http://hcls.deri.org/sparql', 'space'=>'http://api.talis.com/stores/space/services/sparql'}
+  ENDPOINTS = {
+    'bio'=>'http://hcls.deri.org/sparql', 'space'=>'http://api.talis.com/stores/space/services/sparql',
+    'gov'=>'http://semantic.data.gov/sparql', 'dbp'=>'http://dbpedia.org/sparql',
+    'med'=>'http://www4.wiwiss.fu-berlin.de/dailymed/sparql', 'movie'=>'http://data.linkedmdb.org/sparql',
+    'music'=>'http://dbtune.org/musicbrainz/sparql'
+  }
 
-  # @options :type=>{:default=>'classes', :values=>%w{classes objects show_object properties} },
-  #   :endpoint=>{:values=> ENDPOINTS.keys, :default=>'http://api.talis.com/stores/space/services/sparql'},
-  #   :limit=>:numeric, :offset=>:numeric, :abbreviate=>:boolean
+  # @render_options :change_fields=>['name', 'url']
+  # Display endpoints
+  def endpoints
+    ENDPOINTS
+  end
+
+  # @options :type=>{:default=>'classes', :values=>%w{classes objects resource properties} },
+  #   :endpoint=>{:values=> ENDPOINTS.keys, :enum=>false, :default=>'http://api.talis.com/stores/space/services/sparql'},
+  #   :limit=>:numeric, :offset=>:numeric, :abbreviate=>:boolean, :sparql=>{:bool_default=>true, :type=>:string},
+  #   :filters=>:hash
+  # @render_options {}
   # Query and explore a sparql endpoint
   def sparql(*args)
     options = args[-1].is_a?(Hash) ? args.pop : {}
@@ -26,32 +39,45 @@ module RdfLib
     client = SPARQL::Client.new(options[:endpoint])
     if options[:sparql]
       # %[SELECT DISTINCT ?x { [] a ?x }]
-      solutions = client.query(args.join(' '))
-    else
-      query = case options[:type]
-      when 'classes'
-        client.select(:o).where([:s,RDF.type,:o]).distinct
-      when 'objects'
-        # http://purl.org/net/schemas/space/LaunchSite
-        args[0] ?  client.select(:s).where([:s, RDF.type, RDF::URI.new(args[0])]).distinct :
-          client.select(:s).where([:s, :p, :o]).distinct
-      when 'properties'
-        client.select(:p).where([:s, :p, :o]).distinct
-      when 'show_object'
-        # http://nasa.dataincubator.org/launchsite/capecanaveral
-        client.select(:p, :o).where([RDF::URI.new(args[0]), :p, :o])
-      when 'graphs'
-        #select distinct ?Concept ?g where {
-          #GRAPH ?g { [] a ?Concept }
-        #}
+      if options[:sparql] == 'graphs'
+        spl = %[select distinct ?Concept ?g where {
+          GRAPH ?g { [] a ?Concept }
+        }]
+      else
+        spl = args.join(' ')
       end
+      solutions = client.query(spl)
+    else
+      query = select_query(client, options[:type], args)
       query.limit(options[:limit]) if options[:limit]
       query.offset(options[:offset]) if options[:offset]
+      if options[:filters]
+        options[:filters].each {|k,v|
+          query.filter("regex(str(?#{k}), '#{v}', 'i')")
+        }
+      end
       solutions = query.solutions
     end
     results = solutions && solutions.map {|e| e.to_hash }
     abbreviate_uris(results) if options[:abbreviate]
     results
+  end
+
+  def select_query(client, query_type, args)
+    case query_type
+    when 'classes'
+      client.select(:o).where([:s,RDF.type,:o]).distinct
+    when 'objects'
+      # http://purl.org/net/schemas/space/LaunchSite
+      args[0] ?  client.select(:s).where([:s, RDF.type, RDF::URI.new(args[0])]).distinct :
+        client.select(:s).where([:s, :p, :o]).distinct
+    when 'properties'
+      args[0] ?  client.select(:p).where([:s, :p, RDF::URI.new(args[0])]).distinct :
+        client.select(:p).where([:s, :p, :o]).distinct
+    when 'resource'
+      # http://nasa.dataincubator.org/launchsite/capecanaveral
+      client.select(:p, :o).where([RDF::URI.new(args[0]), :p, :o])
+    end
   end
 
   NAMESPACES = {
@@ -67,6 +93,13 @@ module RdfLib
     'sioc'=>'http://rdfs.org/sioc/ns#',
     'po'=>'http://purl.org/ontology/po/',
   }
+
+  # @render_options :change_fields=>['name', 'url']
+  # Display endpoints
+  def namespaces
+    NAMESPACES
+  end
+
   def abbreviate_uris(arr)
     arr.each {|e|
       e.each {|k,v|
