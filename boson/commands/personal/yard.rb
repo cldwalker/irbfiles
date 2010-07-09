07c40da2efd0b48e2d636199154696af53805fca
Gabriel Horner <gabriel.horner@gmail.com>
2010-07-09 16:27:59 -0400

    More convert cmd tweaks

--- boson/commands/personal/yard.rb
+++ boson/commands/personal/yard.rb
@@ -1,15 +1,20 @@
 module YardLib
-  # @options :pretend=>:boolean, :files=>{:type=>:array, :default=>['bin/*', 'lib/**/*.rb'] }
+  # @options :pretend=>:boolean, :files=>{:type=>:array, :default=>['bin/*', 'lib/**/*.rb'] },
+  #  :edits=>:boolean
   # Converts rdoc tags (:nodoc:, :startdoc:, :stopdoc:) to yard's @private
   def convert(options={})
+    total_edits = []
     files = Dir.glob(options[:files])
     files.each do |f|
       edits = []
       (lines = IO.readlines(f)).each_with_index {|e,i|
         if e[/#\s*:nodoc:/]
-          after = !e[/^\s*#:nodoc:/] && lines[i-1][/^\s*#/] ?
-            "# @private\n" + e.sub(/#\s*:nodoc:/, '') :
+          puts "WARNING: Commented class found" if !e[/^\s*#:nodoc:/] && lines[i-1][/^\s*#/] && e[/^\s*class/]
+          after = if !e[/^\s*#:nodoc:/] && lines[i-1][/^(\s*)#/]
+            "#{$1}# @private\n" + e.sub(/#\s*:nodoc:/, '')
+          else
             e.sub(':nodoc:', '@private')
+          end
 
           edits << [e,after,i]
         end
@@ -25,11 +30,13 @@ module YardLib
         edits.each {|before,after,line|
           puts "Line #{line} of #{f}", before.gsub(/^/, "< "), "---", after.gsub(/^/, "> "), ""
         }
+      elsif options[:edits]
+        total_edits += edits
       else
         edits.each {|before,after,l| body = body.sub(before, after) }
         File.open(f, 'w') {|f| f.write body }
       end
     end
-    nil
+    options[:edits] ? total_edits : nil
   end
 end
