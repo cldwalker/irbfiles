b52a76b61553f23302c7639bfb52b38229ea6467
Gabriel Horner <gabriel.horner@gmail.com>
2010-09-05 13:18:54 -0400

    reload_or_require cmd

--- boson/commands/public/misc.rb
+++ boson/commands/public/misc.rb
@@ -1,7 +1,12 @@
 module Misc
-  # Reloads a file just as you would require it.
+  # Reloads a file just as you would require it
   def reload(require_regex)
     $".grep(/#{require_regex}/).each {|e| $".delete(e) && require(e) }
+  end
+
+  # Reloads or requires
+  def reload_or_require(require_regex)
+    require require_regex if reload(require_regex).size.zero?
   end
 
   # From http://solutious.com/blog/2009/09/22/secret-of-object-to_s/
