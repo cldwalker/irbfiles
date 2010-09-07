c780798526cf05677c31e9198c1c23ed9a9c94ac
Gabriel Horner <gabriel.horner@gmail.com>
2010-09-07 18:59:02 -0400

    remove another use of LocalGem

--- irbrc.rb
+++ irbrc.rb
@@ -1,14 +1,5 @@
 # This irbrc uses my irb manager, boson, to load libraries of irb commands + snippets
 
-require 'rubygems'
-%w{hirb alias boson}.each do |e|
-  # load a local gem first or default to normal gem
-  begin
-    require 'local_gem'
-    LocalGem.local_require e
-  rescue LoadError
-    require e
-  end
-end
-
+require 'rubygems' unless ENV['NO_RUBYGEMS']
+require 'boson'
 Boson.start
