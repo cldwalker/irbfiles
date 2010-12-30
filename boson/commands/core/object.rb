66b93da57dd13535264e9320a270adbc78f65cea
Gabriel Horner <gabriel.horner@gmail.com>
2010-12-30 00:26:33 -0500

    add object cmds

--- boson/commands/core/object.rb
+++ boson/commands/core/object.rb
@@ -23,4 +23,26 @@ module ObjectLib
   def original_method(obj, meth, klass=Object)
     klass.instance_method(meth).bind(obj).call
   end
+
+  # From http://d.hatena.ne.jp/namutaka/20101225/1293292912
+  # Print object's ancestors grouping modules by class
+  def show_ancestors(obj)
+    klass = obj.is_a?(Class) ? obj : obj.class
+    inc_mods = klass.included_modules
+    klass.ancestors.each do |c|
+      puts inc_mods.include?(c) ? "-#{c}" : "#{c}"
+    end
+  end
+
+  # From http://d.hatena.ne.jp/namutaka/20101225/1293292912
+  # Grep methods and display where they came from
+  def grep_methods(obj, pattern=nil)
+    klass = obj.is_a?(Class) ? obj : obj.class
+    ancs = klass.ancestors
+    mtds = pattern ? obj.methods.grep(Regexp.new(pattern.to_s)) : obj.methods
+    mtds.sort.each do |e|
+      ent = ancs.find {|c| c.instance_methods(false).include?(e) } || "self"
+      puts "#{e} in #{ent}"
+    end
+  end
 end
