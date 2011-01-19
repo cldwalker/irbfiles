e14bed6b7edd08afd6cb0719d8bd11083e4426bb
Gabriel Horner <gabriel.horner@gmail.com>
2011-01-19 09:27:37 -0500

    more gemcutter cmds

--- boson/commands/public/site/gemcutter.rb
+++ boson/commands/public/site/gemcutter.rb
@@ -44,6 +44,21 @@ module Gemcutter
     json_get "http://gemcutter.org/api/v1/search.json?#{params}"
   end
 
+  # @render_options :fields=> { :values => ["authors", "bug_tracker_uri", "dependencies", "documentation_uri",
+  #  "downloads", "gem_uri", "homepage_uri", "info", "mailing_list_uri", "name", "project_uri", "source_code_uri",
+  #  "version", "version_downloads", "wiki_uri"], :default => ['name', 'version', 'downloads',
+  #  'version_downloads', 'dependencies'] }, :sort => 'downloads', :reverse_sort => true
+  #  List my gems. You must you know your rubygems api key
+  def my_gems(api_key=ENV['RUBYGEMS_API_KEY'])
+    json_get "http://rubygems.org/api/v1/gems.json?api_key=#{api_key}"
+  end
+
+  # @render_options :fields => [:name, :number, :dependencies]
+  # List dependencies of multiple gems
+  def gem_resolve(*rubygems)
+    Marshal.load(get("http://rubygems.org/api/v1/dependencies?gems=#{rubygems.join(',')}"))
+  end
+
   private
   # Gets a json url and converts it to a ruby object
   def json_get(url)
