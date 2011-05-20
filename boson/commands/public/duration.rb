95835dd56141764f1f2f2034d22881e561d9b17e
Gabriel Horner <gabriel.horner@gmail.com>
2011-05-20 10:02:07 -0400

    remove duration and every libs

--- boson/commands/public/duration.rb
+++
@@ -1,7 +0,0 @@
-module Duration
-  def self.included(mod)
-    require 'duration'
-    Object.const_set(:REPL_START_TIME,Time.now)
-    Kernel::at_exit { puts "\nsession duration: #{::Duration.new(Time.now - REPL_START_TIME)}" }
-  end
-end
