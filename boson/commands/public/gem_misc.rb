4179782fe90ad86836ff5d80b97af500084f4b86
Gabriel Horner <gabriel.horner@gmail.com>
2012-05-01 18:25:09 -0400

    finish up re_gem cmd

--- boson/commands/public/gem_misc.rb
+++ boson/commands/public/gem_misc.rb
@@ -1,15 +1,17 @@
 module GemMisc
+  # @option remote, type: :boolean, :desc => 'Install remote dependencies'
   # Uninstall, rebuild and install gem in current directory
-  def re_gem
+  def re_gem(options={})
     rubygem = current_gem
     system("gem uninstall -Iax #{rubygem}")
-    `rake -T`
+    rake = File.exists? 'Gemfile' ? 'bundle exec rake' : 'rake'
+    output = `#{rake} -T 2>/dev/null`
     if $?.success?
-      system("rake gem")
+      output[/rake build/] ? system("rake build") : system("rake gem")
     else
       system("mkdir -p pkg; gem build #{rubygem}.gemspec; mv #{rubygem}-#{version}.gem pkg")
     end
-    system("gem install pkg/#{rubygem}-#{version}.gem")
+    system("gem install pkg/#{rubygem}-#{version}.gem #{options[:remote] ? '' : '--local'}")
   end
 
   # Deletes all gems in current gem list
