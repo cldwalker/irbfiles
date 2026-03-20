56e8926ba6b1ebdb470534a88804bf8c8c15fc93
Gabriel Horner <gabriel.horner@gmail.com>
2026-03-20 13:51:45 -0400

    disable boson till its fixed

--- irbrc.rb
+++ irbrc.rb
@@ -1,5 +1,6 @@
 # This irbrc uses my irb manager, boson, to load libraries of irb commands + snippets
 
 require 'rubygems' unless ENV['NO_RUBYGEMS']
-require 'boson/console'
-Boson.start
+# TODO: Fix boson failing on File.exists?
+# require 'boson/console'
+# Boson.start
